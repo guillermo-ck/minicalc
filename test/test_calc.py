b4d5577c53127133643c2eedc686a2b0bacbd74c
jjsohn92 <kasiossh555@gmail.com>
2025-05-01 22:19:05 +0900

    Add multiply function

--- test/test_calc.py
+++ test/test_calc.py
@@ -8,3 +8,7 @@ class TestCalc(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
 
+    def test_multiply(self):
+        from calc import multiply
+        self.assertEqual(multiply(4, 5), 20)
+
