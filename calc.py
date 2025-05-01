107bddda9ce57a127d8eeafff3ed5e20144309bf
jjsohn92 <kasiossh555@gmail.com>
2025-05-01 22:23:58 +0900

    Add logging to add and subtract functions

--- calc.py
+++ calc.py
@@ -1,7 +1,12 @@
 # calc.py
+import logging
+logging.basicConfig(level=loggin.INFO)
+
 def add(a, b):
+    logging.info(f"Adding {a} + {b}")
     return a + b
 
 def subtract(a, b):
+    logging.info(f"Subtracting {a} - {b}")
     return a - b
 
