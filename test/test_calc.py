e42008ca249ba2069383e6083cd81ba5d0461d29
jjsohn92 <kasiossh555@gmail.com>
2025-05-02 14:13:57 +0900

    Fix an incorrect assertion in test_power

--- test/test_calc.py
+++ test/test_calc.py
@@ -9,4 +9,4 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(subtract(5, 3), 2)
 
     def test_power(self):
-        self.assertEqual(power(2, 2), 8)
+        self.assertEqual(power(2, 3), 8)
