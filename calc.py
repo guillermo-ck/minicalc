6c55977161db14a1486ba5cbdd3d9ff0ce5daefa
jjsohn92 <kasiossh555@gmail.com>
2025-05-02 15:07:58 +0900

    Add division function

--- calc.py
+++ calc.py
@@ -5,3 +5,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+<<<<<<< HEAD
+=======
+def power(a, b):
+    return a ** b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+>>>>>>> 8003a42 (Add division function)
