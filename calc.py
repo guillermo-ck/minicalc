7b119fc01c27f47119c2a676294f57f7296ede17
Guillermo CK <guillermock2@gmail.com>
2025-05-13 01:44:08 +0900

    Fixed mistake from previous commit and added the modified calc.py file

--- calc.py
+++ calc.py
@@ -1,17 +1,7 @@
-# calc.py
+C:\Users\guill\Programitas\sda\minicalc# calc.py
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
 
-<<<<<<< HEAD
-=======
-def power(a, b):
-    return a ** b
-
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
->>>>>>> 8003a42 (Add division function)
